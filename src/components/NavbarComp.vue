5975986697a4dd83014b3a0e443d9a5b6e480b1d
KeenoFourie <keenofourie614@gmail.com>
2024-03-08 12:29:02 +0200

    navbar responsiveness

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-transparent">
   <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -36,5 +36,51 @@
 </script>
 
 <style scoped>
+.business_logo {
+    height: 60px;
+    padding: 5px;
+    padding-right: 20px;
+}
+.navbar-expand-lg .navbar-nav {
+    flex-direction: row;
+    padding: 15px;
+    margin: auto;
+    display: flex;
+    align-items: center;
+}
+.nav-link {
+    padding-left: 25px !important;
+    font-weight: 500 !important;
+    color: #6b6b6b !important;
+}
+.nav-link.active {
+    color: black !important;
+}
 
+@media (width< 991px) {
+  .nav-link {
+    padding-left: 0px !important;
+    font-weight: 500 !important;
+    color: #6b6b6b !important;
+  }
+  img {
+    display: none;
+  }
+  .navbar-expand-lg .navbar-nav {
+    flex-direction: column;
+    padding-left: 0px;
+    margin: auto;
+    display: flex;
+    align-items: center;
+  }    
+  .navbar-toggler {
+    font-size: var(--bs-navbar-toggler-font-size);
+    line-height: 1;
+    color: black !important;
+    background-color: transparent;
+    border: none !important;
+    border-radius: var(--bs-navbar-toggler-border-radius);
+    transition: var(--bs-navbar-toggler-transition);
+  }
+}
 </style>

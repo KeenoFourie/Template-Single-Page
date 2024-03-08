5f43866601f2bed9d93fb48d02430bbae945ae80
KeenoFourie <keenofourie614@gmail.com>
2024-03-08 09:50:42 +0200

    import

--- src/main.js
+++ src/main.js
@@ -4,4 +4,11 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+// CSS
+import "@/assets/css/style.css"
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
 createApp(App).use(store).use(router).mount('#app')

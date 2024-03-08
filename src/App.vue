77512d1559de83a926b41d7cf65c11d47023d1b3
KeenoFourie <keenofourie614@gmail.com>
2024-03-08 10:11:07 +0200

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+<div>
+  <NavbarComp/>
   <router-view/>
+  <FooterComp/>
+</div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import NavbarComp from "@/components/NavbarComp.vue"
+import FooterComp from "@/components/FooterComp.vue"
+export default {
+  components: {
+    NavbarComp,
+    FooterComp
+  }
 }
+</script>
 
-nav {
-  padding: 30px;
-}
+<style scoped>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,10 @@
 import { createStore } from 'vuex'
+// import axios from "axios";
+// import sweet from 'sweetalert';
+// import router from '@/router';
+// import {useCookies} from 'vue3-cookies';
+// const {cookies} = useCookies()
+// import authenticateUser from '@/services/AuthenticateUser';
 
 export default createStore({
   state: {

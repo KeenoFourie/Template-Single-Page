e1598cdba65d62d382ddcc27172a067361d32c41
KeenoFourie <keenofourie614@gmail.com>
2024-03-08 12:14:13 +0200

    navbar styling

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -2,12 +2,14 @@
   <div>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <img src="https://i.postimg.cc/gJ9VyTVh/add-square-icon-2048x2046-oeui0bde.png" alt="business_logo">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <img class="business_logo" src="https://i.postimg.cc/gJ9VyTVh/add-square-icon-2048x2046-oeui0bde.png" alt="business_logo">
+        </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>

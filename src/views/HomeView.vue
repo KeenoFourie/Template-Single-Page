33e51ebf68d22d6903c4a9f15634621681bfacbc
KeenoFourie <keenofourie614@gmail.com>
2024-03-25 08:57:38 +0200

    adding sections

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-
+    <div id="section-1">
+      <h1>Section 1</h1>
+    </div>
+    <div id="section-2">
+      <h1>Section 2</h1>
+    </div>
+    <div id="section-3">
+      <h1>Section 3</h1>
+    </div>
+    <div id="section-4">
+      <h1>Section 4</h1>
+    </div>
   </div>
 </template>
 
@@ -11,5 +22,8 @@
 </script>
 
 <style scoped>
-
+#section-1, #section-2, #section-3, #section-4 {
+  height: 80vh;
+  border: 1px solid black;
+}
 </style>
